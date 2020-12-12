d8b2dc51faed8534b0ec6963d68912945214132c
Squirrel-314 <68402033+Squirrel-314@users.noreply.github.com>
2020-12-12 13:30:03 -0500

    hi

--- search.js
+++ search.js
@@ -1,68 +1,20 @@
-//var userSearch = document.getElementById("search").value;
-// Get the input field
 var input = document.getElementById("search");
 
-// Execute a function when the user releases a key on the keyboard
 input.addEventListener("keyup", function(event) {
-  // Number 13 is the "Enter" key on the keyboard
-  if (event.keyCode === 13) {
-    // Cancel the default action, if needed
-    event.preventDefault();
-    // Trigger the button element with a click
-    document.getElementById("myBtn").click();
-  }
+   if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("myBtn").click();
+   }
 });
 
 
-
-document.getElementById("myBtn").addEventListener("click", run);
 document.getElementById("myBtn").addEventListener("click", hi);
-function run() {
-   let thing;
-}
 
 function hi() {
-   if (thing = null) {
-      document.getElementById("myBtn").innerHTML = "hi";
-   }
+   document.getElementById("myBtn").style.backgroundColor = "red";
 }
 
 
-
-
-
-
-
-
-
-
-/*
-input.addEventListener("keyup", function(event) {
-  if (event.keyCode === 13) {
-     event.preventDefault();
-     document.getElementById('testss').innerHTML = userSearch;
-   }
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-let input = //user search
-
-if (input == "ASAP" || "asap" ){
-   alert("ASAP stands for: As Soon As Possible");
+if (input === "ASAP" || "asap" ){
+   console.log("ASAP stands for: As Soon As Possible");
 }
-*/
