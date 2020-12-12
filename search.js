26ccd7a989a7696e3f7abf1a4f885fd9e1b98562
Squirrel-314 <68402033+Squirrel-314@users.noreply.github.com>
2020-12-12 13:21:16 -0500

    hi

--- search.js
+++ search.js
@@ -1,7 +1,45 @@
-var userSearch = document.getElementById("search").value;
+//var userSearch = document.getElementById("search").value;
+// Get the input field
+var input = document.getElementById("search");
 
-input.addEventListener("keyup", function() {
+// Execute a function when the user releases a key on the keyboard
+input.addEventListener("keyup", function(event) {
+  // Number 13 is the "Enter" key on the keyboard
   if (event.keyCode === 13) {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("myBtn").click();
+  }
+});
+
+
+
+document.getElementById("myBtn").addEventListener("click", run);
+document.getElementById("myBtn").addEventListener("click", hi);
+function run() {
+   let thing;
+}
+
+function hi() {
+   if (thing = null) {
+      document.getElementById("myBtn").innerHTML = "hi";
+   }
+}
+
+
+
+
+
+
+
+
+
+
+/*
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+     event.preventDefault();
      document.getElementById('testss').innerHTML = userSearch;
    }
 });
