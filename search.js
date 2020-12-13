5fc8bd175348fed28fa3afccd72c065d80f328d9
Squirrel-314 <68402033+Squirrel-314@users.noreply.github.com>
2020-12-13 08:28:27 -0500

    hi

--- search.js
+++ search.js
@@ -3,20 +3,19 @@ let input = document.getElementById("search");
 input.addEventListener("keyup", function(event) {
    if (event.keyCode === 13) {
       event.preventDefault();
-      document.getElementById("myBtn").click();
-
+      search();
    }
 });
 
 
-document.getElementById("myBtn").addEventListener("click", hi);
-
-function hi() {
-   document.getElementById("myBtn").style.backgroundColor = "red";
-}
-
-
-if (input = "ASAP"){
-   document.getElementById("about").innerHTML = "HI!";
-   console.log("ASAP stands for: As Soon As Possible");
+function search() {
+   if (input.value == "ASAP"){
+      document.getElementById("about").innerHTML = "ASAP stands for 'As Soon As Possible'";
+   }
+   if (input.value == null) {
+      document.getElementById("about").innerHTML = "No input found.";
+   }
+   else {
+      document.getElementById("about").innerHTML = input.value;
+   }
 }
