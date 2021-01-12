0d16404c2d0ed42299800cbb5bea2e9335842588
Squirrel-314 <schoolarlybay@gmail.com>
2021-01-12 09:58:04 -0500

    Fixed It! Search now works.

--- search.js
+++ search.js
@@ -1,21 +1,25 @@
-let input = document.getElementById("search");
+let input = document.querySelector("#search");
+let result = document.getElementById("about");
 
 input.addEventListener("keyup", function(event) {
    if (event.keyCode === 13) {
       event.preventDefault();
+      document.getElementById("about").innerHTML = " ";
       search();
    }
 });
 
-
 function search() {
-   if (input.value == "ASAP"){
-      document.getElementById("about").innerHTML = "ASAP stands for 'As Soon As Possible'";
+   if (input.value == "ASAP") {
+      result.innerHTML = "ASAP stands for 'As Soon As Possible'";
    }
-   if (input.value == null) {
-      document.getElementById("about").innerHTML = "No input found.";
+   else if (input.value == "LOL") {
+      result.innerHTML = "LOL stands for 'Laugh out Loud' <br> Rare usage: Lots of Love";
+   }
+   else if (input.value == "WTF") {
+      result.innerHTML = "WASH YOUR MOUTH WITH SOAP AND NEVER REPEAT THOSE FOUL WORDS!";
    }
    else {
-      document.getElementById("about").innerHTML = input.value;
+      result.innerHTML = "Sorry, that term is not in out dictionary yet. Please create an issue on <a href=\"https://github.com/Squirrel-314/glish-translate/issues\"  target=\"_blank\">GitHub</a>";
    }
 }
